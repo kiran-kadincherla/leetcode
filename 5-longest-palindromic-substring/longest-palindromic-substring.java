fed34fdffa437f2efe633d155ef391f62b21c540
Kiran565656 <113523491+Kiran565656@users.noreply.github.com>
2025-01-22 23:07:29 +0530

    Time: 15 ms (86.18%) | Memory: 42.6 MB (66.07%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,43 +1,27 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if(s.isEmpty())
-            return s;
-        String result = generateSubString(s);
-        return result;
-    }
+        if (s == null || s.length() < 1) {
+            return "";
+        }
 
-
-    private String generateSubString(String input){
-        String currentPalindrome = "";
-        if(input.isEmpty())
-            return input;
-        for(int i=0;i<input.length();i++){
-            StringBuilder subString = new StringBuilder(String.valueOf(input.charAt(i)));
-            for(int j=i+1;j<input.length();j++){
-                subString.append(input.charAt(j));
-                if (currentPalindrome.length() < subString.length()){
-                    boolean result = checkPalindrome(String.valueOf(subString));
-                    if(result) {
-                        currentPalindrome = String.valueOf(subString);
-                    }
-                }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i); // Odd-length palindromes
+            int len2 = expandAroundCenter(s, i, i + 1); // Even-length palindromes
+            int len = Math.max(len1, len2);
+            if (len > (end - start)) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
             }
         }
-        return currentPalindrome.isEmpty() ? String.valueOf(input.charAt(0)) : currentPalindrome;
+        return s.substring(start, end + 1);
     }
 
-     private boolean checkPalindrome(String word){
-        int ptr1=0;
-        int ptr2=word.length()-1;
-
-        while(ptr1 < ptr2){
-            if (word.charAt(ptr1) != word.charAt(ptr2)) {
-               return false;
-            } else {
-                ptr2--;
-                ptr1++;
-            }
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
         }
-        return true;
+        return right - left - 1;
     }
 }
