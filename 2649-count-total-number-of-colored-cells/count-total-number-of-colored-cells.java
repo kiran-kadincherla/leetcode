a69f6537f50d201405460cdc09d09eea93548226
Kiran565656 <113523491+Kiran565656@users.noreply.github.com>
2025-03-05 23:47:47 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (93.03%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
@@ -1,11 +1,8 @@
 class Solution {
-    public long coloredCells(int n) {
-       int addend = 4;
-       long result = 1;
-       for(int i=1;i<n;i++){
-            result += addend;
-            addend += 4;
-       }
-       return result;
+     public long coloredCells(int n) {
+        //since we are adding 1 + 1*4 + 2*4 + 3*4 --------- + n-1*4. so we can apply sum of natural numbers formula
+        //1+4(n*(n-1)/2)= 1+2*n*(n-1)
+
+        return 1 + 2L *n*(n-1);
     }
 }
