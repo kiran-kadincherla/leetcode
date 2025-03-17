63624bbf78818ac74a0d5ba1cedde85a6c5777f1
Kiran565656 <113523491+Kiran565656@users.noreply.github.com>
2025-03-17 19:06:51 +0530

    Time: 14 ms (0.95%) | Memory: 41.7 MB (8.63%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,24 @@
+class Solution {
+    Map<Integer, Integer> memo;
+    public int rob(int[] nums) {
+        memo = new HashMap<>();
+        return getMaxProfit(nums, 0);
+    }
+
+
+    private int getMaxProfit(int[] nums, int currentIndex) {
+        if(currentIndex >= nums.length){
+            return 0;
+        }
+        System.out.println("Current Index: " + currentIndex);
+        if(memo.containsKey(currentIndex)){
+            return memo.get(currentIndex);
+        } 
+        int pick = nums[currentIndex] + getMaxProfit(nums, currentIndex+2);
+        int skip = getMaxProfit(nums, currentIndex+1);
+
+        int maxProfit = Math.max(pick, skip);
+        memo.put(currentIndex, maxProfit);
+        return maxProfit;
+    }
+}
