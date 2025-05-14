e72c3f054fa9cc876e8093c2308f9af4970072e0
Kiran Kadincherla <113523491+kiran-kadincherla@users.noreply.github.com>
2025-05-14 12:38:13 +0530

    Time: 2006 ms (6.10%) | Memory: 47.1 MB (15.85%) - LeetSync

--- 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.java
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.java
@@ -1,49 +1,52 @@
 class Solution {
 public int lengthAfterTransformations(String s, int t) {
-    final int MOD = 1_000_000_007;
+        Map<Character, String> nextChar = new HashMap<>();
+        Map<Character, Long> charCountPrimary = new HashMap<>();
+        Long[] charCountArr = new Long[26];
+        for (char c = 'a'; c < 'z'; c++) {
+            nextChar.put(c, String.valueOf((char)(c + 1)));
+        }
+        nextChar.put('z', "ab");
 
-    // Store transformation rules: a → b, ..., z → "ab"
-    String[] nextChar = new String[26];
-    for (char c = 'a'; c < 'z'; c++) {
-        nextChar[c - 'a'] = String.valueOf((char) (c + 1));
+        for (char c = 'a'; c <= 'z'; c++) {
+            charCountArr[c-'a'] = 0L;
+        }
+
+        for(char value : s.toCharArray()){
+            charCountArr[value-'a'] = (charCountArr[value-'a'] + 1);
+        }
+
+        for(int i=0;i<t;i++){
+            charCountArr = getCharCount(nextChar, charCountArr);
+        }
+
+        long result = 0;
+        final int MOD = 1_000_000_007;
+        for(int i=0;i<charCountArr.length;i++){
+            result = (result + charCountArr[i]) % MOD;
+        }
+
+        return (int) result;
     }
-    nextChar['z' - 'a'] = "ab";
 
-    // Initialize count of characters from the input string
-    long[] charCount = new long[26];
-    for (char ch : s.toCharArray()) {
-        charCount[ch - 'a']++;
-    }
+    private Long[] getCharCount(Map<Character, String> nextChar, Long[] currentCount){
+        Long[] charCountArr = new Long[26];
+        final int MOD = 1_000_000_007;
 
-    // Apply transformations t times
-    for (int i = 0; i < t; i++) {
-        charCount = transform(charCount, nextChar, MOD);
-    }
+        for (char c = 'a'; c <= 'z'; c++) {
+            charCountArr[c-'a'] = 0L;
+        }
 
-    // Sum all character counts modulo MOD
-    long result = 0;
-    for (long count : charCount) {
-        result = (result + count) % MOD;
-    }
-
-    return (int) result;
-}
-
-private long[] transform(long[] currentCount, String[] nextChar, int MOD) {
-    long[] newCount = new long[26];
-
-    for (int i = 0; i < 26; i++) {
-        long count = currentCount[i];
-        if (count > 0) {
-            String transformed = nextChar[i];
-            for (char ch : transformed.toCharArray()) {
-                newCount[ch - 'a'] = (newCount[ch - 'a'] + count) % MOD;
+        for(int i=0;i<currentCount.length;i++){
+            if(currentCount[i] > 0){
+                String value = nextChar.get((char)(i+'a'));
+                for(char c : value.toCharArray()){
+                    int index = c - 'a';
+                    Long count = currentCount[i] + charCountArr[index];
+                    charCountArr[index] = count % MOD;
+                }
             }
         }
+        return charCountArr;
     }
-
-    return newCount;
-}
-
-
 }
