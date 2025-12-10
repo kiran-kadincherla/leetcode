4543427e61d15af60ee21bbbdbd49dfcb57c6eee
Kiran Kadincherla <113523491+kiran-kadincherla@users.noreply.github.com>
2025-12-10 12:42:13 +0530

    Time: 5 ms (32.76%) | Memory: 54.7 MB (6.52%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,59 +1,44 @@
 class Solution {
-     public int numIslands(char[][] grid) {
-        if(grid.length == 0){
-            return 0;
-        }
-        char[] oneDGrid = new char[grid.length * grid[0].length];
-        int oneDGridIndex = 0;
-        Set<Integer> visitedVertices = new HashSet<>();
-        int result = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                oneDGrid[oneDGridIndex++] = grid[i][j];
+    public int numIslands(char[][] grid) {
+        int noOfIslands = 0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+                if(grid[i][j]=='1'){
+                    List<int[]> currentLands = new ArrayList<>();
+                    currentLands.add(new int[]{i,j});
+                    turnIslands(currentLands, grid);
+                    noOfIslands++;
+                }
             }
         }
-        for (int i = 0; i < oneDGrid.length; i++) {
-            if (oneDGrid[i] == '1' && !visitedVertices.contains(i)) {
-                visitedVertices.addAll(dfs(grid[0].length, oneDGrid, new Stack<Integer>(), i, new HashSet<>()));
-                result++;
+        return noOfIslands;
+    }
+
+    private List<int[]> turnIslands(List<int[]> currentLands, char[][] grid){
+        if(currentLands.isEmpty() || grid.length == 0){
+            return new ArrayList<>();
+        }
+        List<int[]> newLands = new ArrayList<>();
+        for(int[] landIndex : currentLands){
+            int row = landIndex[0];
+            int col = landIndex[1];
+            if(row-1 >= 0 && grid[row-1][col]=='1'){ //left
+                grid[row-1][col]='0';
+                newLands.add(new int[]{row-1, col});
+            }
+            if(row+1 < grid.length && grid[row+1][col]=='1'){ //right
+                grid[row+1][col]='0';
+                newLands.add(new int[]{row+1, col});
+            }
+            if(col+1 < grid[0].length && grid[row][col+1]=='1'){ //bottom
+                grid[row][col+1]='0';
+                newLands.add(new int[]{row, col+1});
+            }
+            if(col-1 >= 0 && grid[0].length-1 >= col-1 && grid[row][col-1]=='1'){ //top
+                grid[row][col-1]='0';
+                newLands.add(new int[]{row, col-1});
             }
         }
-        return result;
+        return turnIslands(newLands, grid);
     }
-
-    private Set<Integer> dfs(int gridRowSize, char[] oneDGrid, Stack<Integer> verticesStack, int currentVerticeIndex, Set<Integer> visitedVertices) {
-        visitedVertices.add(currentVerticeIndex);
-        int connectedVertexIndex = getConnectedVertexIndex(currentVerticeIndex, visitedVertices, gridRowSize, oneDGrid);
-        if (connectedVertexIndex == -1) {
-            if (!verticesStack.isEmpty()) {
-                return dfs(gridRowSize, oneDGrid, verticesStack, verticesStack.pop(), visitedVertices);
-            }
-            return visitedVertices;
-        }
-        verticesStack.push(currentVerticeIndex);
-        return dfs(gridRowSize, oneDGrid, verticesStack, connectedVertexIndex, visitedVertices);
-    }
-
-    private int getConnectedVertexIndex(int currentVerticeIndex, Set<Integer> visitedVertices, int gridRowSize, char[] oneDGrid) {
-    int col = currentVerticeIndex % gridRowSize;
-
-    // left
-    if (col > 0 && oneDGrid[currentVerticeIndex - 1] == '1' && !visitedVertices.contains(currentVerticeIndex - 1)) {
-        return currentVerticeIndex - 1;
-    }
-    // right
-    if (col < gridRowSize - 1 && oneDGrid[currentVerticeIndex + 1] == '1' && !visitedVertices.contains(currentVerticeIndex + 1)) {
-        return currentVerticeIndex + 1;
-    }
-    // down
-    if (currentVerticeIndex + gridRowSize < oneDGrid.length && oneDGrid[currentVerticeIndex + gridRowSize] == '1' && !visitedVertices.contains(currentVerticeIndex + gridRowSize)) {
-        return currentVerticeIndex + gridRowSize;
-    }
-    // up
-    if (currentVerticeIndex - gridRowSize >= 0 && oneDGrid[currentVerticeIndex - gridRowSize] == '1' && !visitedVertices.contains(currentVerticeIndex - gridRowSize)) {
-        return currentVerticeIndex - gridRowSize;
-    }
-
-    return -1;
-}
 }
