352c095b4c24b957ddefc67b162a2b7bc068f308
Kiran Kadincherla <113523491+kiran-kadincherla@users.noreply.github.com>
2026-04-03 18:16:28 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (88.30%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -1,34 +1,36 @@
 class Solution {
-    int[] memo;
+    int memo[];
     public int mincostTickets(int[] days, int[] costs) {
-        memo = new int[days[days.length-1]];
+        memo = new int[days.length];
         Arrays.fill(memo, -1);
         return getMinCost(0, days, costs);
     }
 
     private int getMinCost(int currentDayIndex, int[] days, int[] costs){
-        if(currentDayIndex >= days.length){
+        if(currentDayIndex==days.length){
             return 0;
         }
         if(memo[currentDayIndex]!=-1){
             return memo[currentDayIndex];
         }
-        int oneDaypassPrice = costs[0] + getMinCost(currentDayIndex+1, days, costs);
-        int sevenDayIndex = getIndex(days, days[currentDayIndex]+7);
-        int sevenDayPassPrice = costs[1] + getMinCost(sevenDayIndex, days, costs);
-        int thirtyDayIndex = getIndex(days, days[currentDayIndex]+30);
-        int thirtyDayPassPrice = costs[2] + getMinCost(thirtyDayIndex, days, costs);
-        int temp = Math.min(oneDaypassPrice, sevenDayPassPrice);
-        int finalResult = Math.min(temp, thirtyDayPassPrice);
-        memo[currentDayIndex] = finalResult;
-        return finalResult;
+        int oneDay = costs[0]+getMinCost(currentDayIndex+1, days, costs);
+        int sevenDays = costs[1]+getMinCost(getIndex(days, currentDayIndex, days[currentDayIndex]+6), days, costs);
+        int thirtyDays = costs[2]+getMinCost(getIndex(days, currentDayIndex, days[currentDayIndex]+29), days, costs);
+        int min = Math.min(oneDay, sevenDays);
+        int result = Math.min(min, thirtyDays);
+        memo[currentDayIndex] = result;
+        return result;
     }
 
-    private int getIndex(int[] days, int maxVal){
-        int index = 0;
-        while(index < days.length && days[index] < maxVal){
-            index++;
+    private int getIndex(int[] days, int currentIndex, int maxValue){
+        for(int i=currentIndex;i<days.length;i++){
+            if(days[i]>maxValue){
+                return i;
+            }
         }
-        return index;
+        return days.length;
+
     }
+
+   
 }
