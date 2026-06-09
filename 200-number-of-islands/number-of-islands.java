5d50745ca6b309e64f22266742d5ec800113d400
Kiran Kadincherla <113523491+kiran-kadincherla@users.noreply.github.com>
2026-06-09 23:53:49 +0530

    Time: 3 ms (87.18%) | Memory: 51.5 MB (86.77%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,51 +1,31 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int noOfIslands = 0;
+        int result = 0;
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[i].length;j++){
                 if(grid[i][j]=='1'){
-                    dfs(i,j,grid);
-                    noOfIslands++;
+                    fillIsland(grid, i,j);
+                    //System.out.println("..x.."+i+"..y.."+j);
+                    result++;
                 }
             }
         }
-        return noOfIslands;
+        return result;
     }
 
-    private List<int[]> turnIslands(List<int[]> currentLands, char[][] grid){
-        if(currentLands.isEmpty() || grid.length == 0){
-            return new ArrayList<>();
-        }
-        List<int[]> newLands = new ArrayList<>();
-        for(int[] landIndex : currentLands){
-            int row = landIndex[0];
-            int col = landIndex[1];
-            if(row-1 >= 0 && grid[row-1][col]=='1'){ //left
-                grid[row-1][col]='0';
-                newLands.add(new int[]{row-1, col});
+    private void fillIsland(char[][] grid, int x, int y){
+        //System.out.println("..x.."+x+"..y.."+y);
+        if((x >= 0 && x < grid.length) && (y>=0 && y<grid[0].length)){
+            if(grid[x][y]=='1') {
+                grid[x][y]='2';
+                fillIsland(grid, x-1,y); //top
+                fillIsland(grid, x,y-1); //left
+                fillIsland(grid, x,y+1); //right
+                fillIsland(grid, x+1,y); //left
             }
-            if(row+1 < grid.length && grid[row+1][col]=='1'){ //right
-                grid[row+1][col]='0';
-                newLands.add(new int[]{row+1, col});
-            }
-            if(col+1 < grid[0].length && grid[row][col+1]=='1'){ //bottom
-                grid[row][col+1]='0';
-                newLands.add(new int[]{row, col+1});
-            }
-            if(col-1 >= 0 && grid[0].length-1 >= col-1 && grid[row][col-1]=='1'){ //top
-                grid[row][col-1]='0';
-                newLands.add(new int[]{row, col-1});
-            }
-        }
-        return turnIslands(newLands, grid);
+        } 
+        return;
     }
 
-    void dfs(int r, int c, char[][] grid) {
-    if (r<0 || c<0 || r>=grid.length || c>=grid[0].length || grid[r][c]=='0') return;
-    grid[r][c] = '0';
-    dfs(r+1, c, grid);
-    dfs(r-1, c, grid);
-    dfs(r, c+1, grid);
-    dfs(r, c-1, grid);
-    }
+
 }
