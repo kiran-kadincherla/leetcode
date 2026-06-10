2c9047d1a4690a5c307718c03f322b843dbd5700
Kiran Kadincherla <113523491+kiran-kadincherla@users.noreply.github.com>
2026-06-11 00:10:34 +0530

    Time: 44 ms (3.68%) | Memory: 52.4 MB (5.53%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,64 +1,83 @@
 class Solution {
-     public int orangesRotting(int[][] grid) {
-        int[] oneDGrid = new int[grid.length*grid[0].length];
-        int oneDGridIndex = 0;
+    int maxRow;
+    int maxCol;
+    public int orangesRotting(int[][] grid) {
+        Queue<Integer> indicesQueue = new LinkedList<>();
+        List<Integer> rottenIndeces = new ArrayList<>();
+        maxRow = grid.length;
+        maxCol = grid[0].length;
+        int result = 0;
+        
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[i].length;j++){
-                oneDGrid[oneDGridIndex++] = grid[i][j];
+                if(grid[i][j]==2){
+                    rottenIndeces.add(getOneDArrayIndexCount(i,j));
+                }
             }
         }
-        Map<String, Object> tomatoData = getTomatoDetails(oneDGrid);
-        List<Integer> rottenIndexes = (List<Integer>) tomatoData.get("rottenIndexes");
-        Integer noOfFreshOranges = (Integer) tomatoData.get("noOfFreshOranges");
-        int result = 0;
 
-        while(noOfFreshOranges > 0){
-            for(Integer index : rottenIndexes) {
-                rot(oneDGrid, index, grid[0].length);
+        for(Integer index : rottenIndeces)
+            indicesQueue.offer(index);
+        
+
+
+        while(!indicesQueue.isEmpty()){
+            rottenIndeces = new ArrayList<>();
+            while(!indicesQueue.isEmpty()){
+                Integer index = indicesQueue.poll();
+                rottenIndeces.addAll(fill(grid, index/maxCol, index%maxCol));
             }
-            tomatoData = getTomatoDetails(oneDGrid);
-            rottenIndexes = (List<Integer>) tomatoData.get("rottenIndexes");
-            Integer tempNoOfFreshOranges = (Integer) tomatoData.get("noOfFreshOranges");
-            if(noOfFreshOranges == tempNoOfFreshOranges){
-                return -1;
-            } else {
-                noOfFreshOranges = tempNoOfFreshOranges;
+            
+            if(rottenIndeces.size()>0){
+                System.out.println("resultttt."+result+" ..rottenIndeces.."+rottenIndeces);
+                for(Integer index : rottenIndeces)
+                    indicesQueue.offer(index);
+                result++;
             }
-            result++;
+            rottenIndeces = new ArrayList<>();
+            
         }
+
+
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
+            }
+        }
+
 
         return result;
+        
     }
 
-    private void rot(int[] oneDGrid, int index, int rowLength){
-        if(index-1 >=0 && index/rowLength == ((index-1)/rowLength) && oneDGrid[index-1] == 1){ //left
-            oneDGrid[index-1]=2;
-        }
-        if(index+1 < oneDGrid.length && index/rowLength == ((index+1)/rowLength) && oneDGrid[index+1] == 1){ //right
-            oneDGrid[index+1]=2;
-        }
-        if(index-rowLength >=0 && oneDGrid[index-rowLength] == 1){ //top
-            oneDGrid[index-rowLength]=2;
-        }
-        if(index+rowLength < oneDGrid.length && oneDGrid[index+rowLength] == 1){ //bottom
-            oneDGrid[index+rowLength]=2;
-        }
 
-    }
-
-    private Map<String, Object> getTomatoDetails(int[] oneDGrid) {
-        List<Integer> rottenIndexes = new ArrayList<>();
-        Integer noOfFreshOranges = 0;
-        Map<String, Object> tomatoData = new HashMap<>();
-        for(int i=0;i<oneDGrid.length;i++){
-            if(oneDGrid[i] == 2){
-                rottenIndexes.add(i);
-            } else if(oneDGrid[i] == 1) {
-                noOfFreshOranges++;
+    private List<Integer> fill(int[][] grid, int i, int j){
+        System.out.println("rack.."+i+"..j.."+j);
+        List<Integer> rottenIndeces = new ArrayList<>();
+            if(i-1>=0 && grid[i-1][j]==1){ //top
+                grid[i-1][j]=2;
+                rottenIndeces.add(getOneDArrayIndexCount(i-1,j));
             }
-        }
-        tomatoData.put("rottenIndexes", rottenIndexes);
-        tomatoData.put("noOfFreshOranges", noOfFreshOranges);
-        return tomatoData;
+            if(i+1<grid.length && grid[i+1][j]==1){ //bottom
+                grid[i+1][j]=2;
+                rottenIndeces.add(getOneDArrayIndexCount(i+1,j));
+            }
+            if(j-1>=0 && grid[i][j-1]==1){ //left
+                grid[i][j-1]=2;
+                rottenIndeces.add(getOneDArrayIndexCount(i,j-1));
+            }
+            if(j+1<grid[0].length && grid[i][j+1]==1){ //right
+                grid[i][j+1]=2;
+                rottenIndeces.add(getOneDArrayIndexCount(i,j+1));
+            }
+            return rottenIndeces;
+    }
+
+    private int getOneDArrayIndexCount(int i, int j){
+            int index = (i * maxCol) + j;
+            System.out.println("final index."+ index);
+            return index;
     }
 }
