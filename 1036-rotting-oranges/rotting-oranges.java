1990d9aeb9dbbe6b49abd3827178c62073ed5594
Kiran Kadincherla <113523491+kiran-kadincherla@users.noreply.github.com>
2026-06-11 07:28:50 +0530

    Time: 2 ms (90.77%) | Memory: 44.4 MB (24.90%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,8 +1,9 @@
 class Solution {
     int maxRow;
     int maxCol;
+    Queue<int[]> indicesQueue;
     public int orangesRotting(int[][] grid) {
-        Queue<Integer> indicesQueue = new LinkedList<>();
+        indicesQueue = new LinkedList<>();
         List<Integer> rottenIndeces = new ArrayList<>();
         maxRow = grid.length;
         maxCol = grid[0].length;
@@ -11,31 +12,19 @@ class Solution {
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[i].length;j++){
                 if(grid[i][j]==2){
-                    rottenIndeces.add(getOneDArrayIndexCount(i,j));
+                    indicesQueue.offer(new int[]{i,j});
                 }
             }
         }
 
-        for(Integer index : rottenIndeces)
-            indicesQueue.offer(index);
-        
-
-
         while(!indicesQueue.isEmpty()){
-            rottenIndeces = new ArrayList<>();
-            while(!indicesQueue.isEmpty()){
-                Integer index = indicesQueue.poll();
-                rottenIndeces.addAll(fill(grid, index/maxCol, index%maxCol));
+            int size = indicesQueue.size();
+            for(int i=0;i<size;i++){
+                int[] index = indicesQueue.poll();
+                rottenIndeces.addAll(fill(grid, index[0], index[1]));
             }
-            
-            if(rottenIndeces.size()>0){
-                //System.out.println("resultttt."+result+" ..rottenIndeces.."+rottenIndeces);
-                for(Integer index : rottenIndeces)
-                    indicesQueue.offer(index);
+            if(!indicesQueue.isEmpty())
                 result++;
-            }
-            rottenIndeces = new ArrayList<>();
-            
         }
 
 
@@ -58,19 +47,19 @@ class Solution {
         List<Integer> rottenIndeces = new ArrayList<>();
             if(i-1>=0 && grid[i-1][j]==1){ //top
                 grid[i-1][j]=2;
-                rottenIndeces.add(getOneDArrayIndexCount(i-1,j));
+                indicesQueue.offer(new int[]{i-1,j});
             }
             if(i+1<grid.length && grid[i+1][j]==1){ //bottom
                 grid[i+1][j]=2;
-                rottenIndeces.add(getOneDArrayIndexCount(i+1,j));
+                indicesQueue.offer(new int[]{i+1,j});
             }
             if(j-1>=0 && grid[i][j-1]==1){ //left
                 grid[i][j-1]=2;
-                rottenIndeces.add(getOneDArrayIndexCount(i,j-1));
+                indicesQueue.offer(new int[]{i,j-1});
             }
             if(j+1<grid[0].length && grid[i][j+1]==1){ //right
                 grid[i][j+1]=2;
-                rottenIndeces.add(getOneDArrayIndexCount(i,j+1));
+                indicesQueue.offer(new int[]{i,j+1});
             }
             return rottenIndeces;
     }
