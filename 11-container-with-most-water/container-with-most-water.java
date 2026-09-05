1d86876445018ac9ffdfa66220e2cc4cf698adb2
Kiran Kadincherla <113523491+kiran-kadincherla@users.noreply.github.com>
2026-09-05 13:04:56 +0530

    Time: 5 ms (83.25%) | Memory: 77.3 MB (49.13%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maxArea(int[] height) {
+        //since it is greedy approach our aim is to always look for better result after updating the resut we will be moving the pointer whose value is lesser
         int left = 0;
         int right = height.length-1;
         int result = 0;
