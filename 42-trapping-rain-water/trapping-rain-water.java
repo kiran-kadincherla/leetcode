e9dbc6370b73545474a6570dedda4b99faf5797e
Kiran Kadincherla <113523491+kiran-kadincherla@users.noreply.github.com>
2026-09-17 11:51:07 +0530

    Time: 1 ms (56.06%) | Memory: 48.8 MB (8.77%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,36 +1,27 @@
 class Solution {
     public int trap(int[] height) {
-        int[] prefixMax = new int[height.length];
-        int[] postfixMax = new int[height.length];
-        int prev = height[0];
-        int result = 0;
-
+        int[] leftMax = new int[height.length];
+        int[] rightMax = new int[height.length];
+        int left=0;
+        int right=0;
+        int result=0;
         for(int i=0;i<height.length;i++){
-            if(height[i]>prev){
-                prefixMax[i]=height[i];
-                prev=height[i];
-            } else {
-                prefixMax[i]=prev;
-            }
+            left = Math.max(left, height[i]);
+            leftMax[i]=left;
         }
-        prev = height[height.length-1];
         for(int i=height.length-1;i>=0;i--){
-            if(height[i]>prev){
-                postfixMax[i]=height[i];
-                prev=height[i];
-            } else {
-                postfixMax[i]=prev;
-            }
+            right = Math.max(right, height[i]);
+            rightMax[i]=right;
         }
-
         for(int i=0;i<height.length;i++){
-            //System.out.println("i.. "+i+" prefixMax[i].. "+prefixMax[i]+" .. postfixMax[i] .. "+postfixMax[i]);
-            result += Math.min(prefixMax[i], postfixMax[i])-height[i];
+            
+            int min = Math.min(leftMax[i], rightMax[i]);
+            int count = min-height[i];
+            
+            result += min-height[i];
+            //System.out.println(" ..i.. "+leftMax[i] + " .. "+rightMax[i]+" .. "+ " .. " + count + " .. "+ result);
         }
-
+        
         return result;
-
-
-    
     }
 }
