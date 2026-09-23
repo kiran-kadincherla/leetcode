1e3a4fb6da8cb21a94b615894a7b8f0116eea59f
Kiran Kadincherla <113523491+kiran-kadincherla@users.noreply.github.com>
2026-09-23 11:15:50 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (65.90%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,28 +1,56 @@
 class Solution {
-    int target;
     public int search(int[] nums, int target) {
-        this.target = target;
-        return binarySearch(nums, 0, nums.length-1);
+        return binarySearch(nums, target);
     }
 
-    private int binarySearch(int[] arr, int low,int high){
-        int mid = (low+high)/2;
-        if(arr[mid] == target){
-            return mid;
-        }
-        if(low == high){
-            return -1;
-        }
-        if(arr[mid] >=  arr[low] && (target>=arr[low] && target<arr[mid] )){ //left half is sorted
-            return binarySearch(arr, low, mid-1);
-        } else if(arr[mid] <=  arr[high] && (target>arr[mid] && target<=arr[high])) { //right half is sorted
-            return binarySearch(arr, mid+1, high);
-        } else {
-            if(arr[mid] >=  arr[low]){
-                return binarySearch(arr, mid+1, high);
-            } else {
-                return binarySearch(arr, low, mid-1);
+    private int binarySearch(int[] nums, int target){
+        int start = 0;
+        int end=nums.length-1;
+        int result = -1;
+        while(start <= end){
+            int mid = start + ((end-start)/2);
+            if(nums[mid]==target){
+                result = mid;
+                break;
+            } else if(nums[start] <= nums[mid]){ //left side sorted 
+                if(target >= nums[start] && target < nums[mid]) //item present in sorted side
+                    end = mid-1;
+                else {
+                    start = mid+1; // we will check on the right
+                }
+            } else { //right sidee is sorted
+                if(target > nums[mid] && target <= nums[end]){ //item present on sorted side
+                    start = mid+1;
+                } else { //item present on unsorted side i.e.., left side
+                    end = mid-1;
+                }
             }
         }
+        return result;
     }
+
+
+    // bs(nums, target){
+    //     int start = 0;
+    //     end=nums.length-1;
+    //     while(start <= end){
+    //         int mid = end - ((end+start)/2);
+    //         if(nums[mid]==target){
+    //             return mid;
+    //         } else if(nums[start] <= nums[mid]){ //left side sorted 
+    //             if(target >= nums[start] && target <= nums[mid-1]) //item present in sorted side
+    //                 end = mid-1;
+    //             else {
+    //                 start = mid+1; // we will check on the right
+    //             }
+    //         } else { //right sidee is sorted
+    //             if(target >= nums[mid+1] && target <= nums[end]){ //item present on sorted side
+    //                 start = mid+1;
+    //             } else { //item present on unsorted side i.e.., left side
+    //                 end = mid-1;
+    //             }
+    //         }
+    //     }
+    // }
+    
 }
