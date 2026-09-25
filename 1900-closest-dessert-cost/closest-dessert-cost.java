39a9c8f01dd16f275db085e4f0552e6b4408ccb0
Kiran Kadincherla <113523491+kiran-kadincherla@users.noreply.github.com>
2026-09-25 19:56:57 +0530

    Time: 13 ms (49.06%) | Memory: 42.5 MB (92.45%) - LeetSync

---
+++ 1900-closest-dessert-cost/closest-dessert-cost.java
@@ -0,0 +1,24 @@
+class Solution {
+    int result = Integer.MAX_VALUE;
+    public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
+        for(int i=0;i<baseCosts.length;i++){
+            getClosestTarget(toppingCosts, target, baseCosts[i], 0);
+        }
+        return result;
+    }
+
+    private void getClosestTarget(int[] toppingCosts, int target, int currentSum, int currentIndex){
+        if(currentIndex >= toppingCosts.length){
+            int diff = Math.abs(target-currentSum);
+            int currentDiff = Math.abs(target-result);
+            if(diff < currentDiff || (diff==currentDiff && currentSum < result)){
+                result = currentSum;
+            }
+            return;
+        }
+        //System.out.println("index .. "+ currentIndex);
+        getClosestTarget(toppingCosts, target, currentSum, currentIndex+1);
+        getClosestTarget(toppingCosts, target, currentSum+toppingCosts[currentIndex], currentIndex+1);
+        getClosestTarget(toppingCosts, target, currentSum+toppingCosts[currentIndex]+toppingCosts[currentIndex], currentIndex+1);
+    }
+}
